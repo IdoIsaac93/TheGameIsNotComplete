6ec6b38fc4b9c231482f3de6157c170145cb58e5
Raziel1991 <33969670+Raziel1991@users.noreply.github.com>
2025-04-06 15:05:02 -0400

    Pause Menu Working for mobile devices.

--- The Game Is Not Complete/Assets/_Project/Scripts/UI/Pause/PauseMenuEvent.cs
+++ The Game Is Not Complete/Assets/_Project/Scripts/UI/Pause/PauseMenuEvent.cs
@@ -11,7 +11,10 @@ public class PauseMenuEvent : MonoBehaviour
     private Button _mainMenuButton;
     private Button _newGameButton;
     private Button _exitGameButton;
+    private Button _pauseMenuButton;
+    private Button _closeButton;
     private AudioSource _audioSource;
+    private VisualElement _menuMainContainer;
     private List<Button> _listButtons = new List<Button>();
     private bool _isPaused = false;
     private GameObject _canvasUI;
@@ -19,7 +22,7 @@ public class PauseMenuEvent : MonoBehaviour
     {
         _uiDocument = GetComponent<UIDocument>();
         _audioSource = GetComponent<AudioSource>();
-
+        _menuMainContainer = _uiDocument.rootVisualElement.Q<VisualElement>("MainContainer");
 
         if (_uiDocument == null)
         {
@@ -67,7 +70,9 @@ public class PauseMenuEvent : MonoBehaviour
         _mainMenuButton = null;
         _newGameButton = null;
         _exitGameButton = null;
-        
+        _pauseMenuButton = null;
+        _closeButton = null;
+
         _listButtons.Clear();
 
         if (_uiDocument == null || _uiDocument.rootVisualElement == null) return;
@@ -77,7 +82,9 @@ public class PauseMenuEvent : MonoBehaviour
         _mainMenuButton = _uiDocument.rootVisualElement.Q<Button>("MainMenu");
         _newGameButton = _uiDocument.rootVisualElement.Q<Button>("NewGame");
         _exitGameButton = _uiDocument.rootVisualElement.Q<Button>("ExitGame");
-        
+        _pauseMenuButton = _uiDocument.rootVisualElement.Q<Button>("Pause");
+        _closeButton = _uiDocument.rootVisualElement.Q<Button>("CloseMenu");
+
 
         if (_mainMenuButton != null)
         {
@@ -97,6 +104,19 @@ public class PauseMenuEvent : MonoBehaviour
             _exitGameButton.clicked -= OnExitGameClick;
             _exitGameButton.clicked += OnExitGameClick;
         }
+
+        if (_pauseMenuButton != null)
+        {
+            _pauseMenuButton.clicked -= OnPauseMenuButton;
+            _pauseMenuButton.clicked += OnPauseMenuButton;
+        }
+
+        if (_closeButton != null)
+        {
+            _closeButton.clicked -= OnCloseMenubutton;
+            _closeButton.clicked += OnCloseMenubutton;
+        }
+
 
         // Get all buttons and register the common click event
         _listButtons = _uiDocument.rootVisualElement.Query<Button>().ToList();
@@ -133,8 +153,12 @@ public class PauseMenuEvent : MonoBehaviour
             }
         }
 
-        _uiDocument.rootVisualElement.style.display = DisplayStyle.Flex;
-        _uiDocument.rootVisualElement.style.visibility = Visibility.Visible;
+        _menuMainContainer.style.display = DisplayStyle.Flex;
+        _menuMainContainer.style.visibility = Visibility.Visible;
+        _menuMainContainer.AddToClassList("bottomsheet-show");
+
+        //_uiDocument.rootVisualElement.style.display = DisplayStyle.Flex;
+        //_uiDocument.rootVisualElement.style.visibility = Visibility.Visible;
 
         // Ensure all buttons are initialized and event handlers are attached
         InitializeButtons();
@@ -143,11 +167,16 @@ public class PauseMenuEvent : MonoBehaviour
     public void HidePauseMenu()
     {
         
-
+        
         if (_uiDocument != null && _uiDocument.rootVisualElement != null)
         {
-            _uiDocument.rootVisualElement.style.display = DisplayStyle.None;
-            _uiDocument.rootVisualElement.style.visibility = Visibility.Hidden;
+            _menuMainContainer.style.display = DisplayStyle.None;
+            _menuMainContainer.style.visibility = Visibility.Hidden;
+            _menuMainContainer.AddToClassList("bottomsheet");
+            //_uiDocument.rootVisualElement.style.display = DisplayStyle.None;
+            //_uiDocument.rootVisualElement.style.visibility = Visibility.Hidden;
+            //uiDocument.rootVisualElement.style.display = DisplayStyle.None;
+
         }
 
         
@@ -191,13 +220,7 @@ public class PauseMenuEvent : MonoBehaviour
 
 
     /// Buttons Click Events
-    private void OnRestartLevelClick()
-    {
-        Debug.Log("Restart Game clicked");
-        // Set timeScale back to 1 immediately to ensure UI responsiveness
-        Time.timeScale = 1f;
-        DataPersistanceManager.Instance.NewGame(SceneManager.GetActiveScene().buildIndex);    
-    }
+
 
     private void OnMainMenuClick()
     {
@@ -217,19 +240,33 @@ public class PauseMenuEvent : MonoBehaviour
 
     }
 
-    private void OnLoadGameClick()
-    {
-        // Set timeScale back to 1 immediately to ensure UI responsiveness
-        Time.timeScale = 1f;
-        DataPersistanceManager.Instance.LoadGame();
-    }
-
     private void OnExitGameClick()
     {
         Debug.Log("Exit Game clicked");
         // Set timeScale back to 1 immediately
         Time.timeScale = 1f;
         Application.Quit();
+    }
+
+    private void OnPauseMenuButton()
+    {
+        Debug.Log("Pause Menu Button clicked  or touched");
+        // Set timeScale back to 1 immediately to ensure UI responsiveness
+        Time.timeScale = 0f;
+        _pauseMenuButton.style.display = DisplayStyle.None;
+        
+        ShowPauseMenu();
+    }
+
+    private void OnCloseMenubutton()
+    {
+        _menuMainContainer.RemoveFromClassList("bottomsheet-show");
+        _menuMainContainer.style.display = DisplayStyle.None;
+        _menuMainContainer.style.visibility = Visibility.Hidden;
+        _pauseMenuButton.style.display = DisplayStyle.Flex;
+        
+        Time.timeScale = 1f;
+        
     }
 
     private void OnAllButtonsClick()
